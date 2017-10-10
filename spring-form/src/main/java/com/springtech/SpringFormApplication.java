be08098f9d76dc60ceb328d8f4fb7c0eacea93e5
Vetriselvan1187 <vetriblogger1187@gmail.com>
2017-10-10 17:14:40 +0530

    new spring form project

---
+++ spring-form/src/main/java/com/springtech/SpringFormApplication.java
@@ -0,0 +1,12 @@
+package com.springtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFormApplication.class, args);
+	}
+}
